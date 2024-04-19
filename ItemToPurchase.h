9935d3601d57d604f673a625796a1cb4142857eb
krhickssmu <167340617+krhickssmu@users.noreply.github.com>
2024-04-19 11:27:03 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -5,5 +5,33 @@
 using namespace std;
 
 /* Type your code here */
+class ItemToPurchase{
+    private:
+
+    //data members
+    string itemName;
+    int itemPrice;
+    int itemQuantity;
+
+    public:
+
+    //constructor
+    ItemToPurchase();
+
+    //mutators
+    void SetName(string name);
+    void SetPrice(int price);
+    void SetQuantity(int quantity);
+
+    //accessors
+    string GetName();
+    int GetPrice();
+    int GetQuantity();
+    
+}
+/* Type your code here */
+
+#endif
+
 
 #endif
