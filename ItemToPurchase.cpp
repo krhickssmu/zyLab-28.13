e3504e4d0989f251eaac5035ae052d8816584545
RuthA120 <167146819+RuthA120@users.noreply.github.com>
2024-04-19 11:38:23 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -3,3 +3,34 @@ using namespace std;
 
 #include "ItemToPurchase.h"
 
+
+string ItemToPurchase::GetName(){
+    return itemName;
+}
+
+int ItemToPurchase::GetPrice(){
+    return itemPrice;
+}
+
+int ItemToPurchase::GetQuantity(){
+    return itemQuantity;
+}
+
+void ItemToPurchase::SetName(string itemName){
+    this->itemName = itemName;
+}
+
+void ItemToPurchase::SetPrice(int itemPrice){
+    this->itemPrice = itemPrice;
+}
+
+void ItemToPurchase::SetQuantity(int itemQuantity){
+    this->itemQuantity = itemQuantity;
+}
+
+ItemToPurchase::ItemToPurchase(string itemName = "none", int itemPrice = 0, int itemQuantity = 0){
+  this->itemName = itemName;
+  this->itemPrice = itemPrice;
+  this->itemQuantity = itemQuantity;
+}
+
